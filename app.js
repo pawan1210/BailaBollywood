c8121e686ae5c140adf1eb0ab8443c76e1a52164
Pawan Saggu <pawansaggu1210@gmail.com>
2020-03-09 02:31:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const middleware = require('./middleware/index');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const Promise = require('promise');
-const port =  80;
+const port =  3000;
 
 
 
